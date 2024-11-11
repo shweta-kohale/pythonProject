6441f02536d61d35aabc702abd7896978070cde5
A1 <shweta.kohale@gmail.com>
2024-11-11 21:17:47 +0530

    new programs

---
+++ ps_09112024_HomeWork/Lab_09112024_Task2.py
@@ -0,0 +1,17 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = int(input("Enter third number"))
+sum = a+b+c
+sub = a-b-c
+mul = a*b*c
+div = a/b/c
+
+
+print("sum is ",sum)
+print("sub is ",sub)
+print("mul is ",mul)
+print("div is ",div)
