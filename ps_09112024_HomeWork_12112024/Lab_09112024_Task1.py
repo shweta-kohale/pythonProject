6a17d3397a9a2d8ed6fc6d111181c44a9d3364da
A1 <shweta.kohale@gmail.com>
2024-11-12 08:14:58 +0530

    Homework

---
+++ ps_09112024_HomeWork_12112024/Lab_09112024_Task1.py
@@ -0,0 +1,15 @@
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+Quotient = a/b
+Remainder = a%b
+print("Quotient is ",int(Quotient))
+print("Remainder is ",Remainder)
+print(f"Quotient is {Quotient:2f}")
+print(f"Remainder {Remainder}")
